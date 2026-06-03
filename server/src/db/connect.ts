9fd6fb8df3582b30f776c13a05dc486f78d48839
rut2159 <ruti4269@gmail.com>
2026-06-03 16:51:58 +0300

    connect to DB

--- server/src/db/connect.ts
+++ server/src/db/connect.ts
@@ -1,11 +1,12 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 export async function connectDB() {
     try {
         // כאן אנחנו משתמשים בכתובת שתשימי ב-.env
-        await mongoose.connect(process.env.MONGO_URI as string);
+          await mongoose.connect(process.env.MONGO_URI!);
         console.log('✅ Successfully connected to MongoDB Atlas!');
     } catch (error) {
         console.error('❌ Error connecting to MongoDB:', error);
